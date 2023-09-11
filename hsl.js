f85a218df392b62d4d2faaa7482b6a5c68be175a
lsburtonBYU <lsburton@byu.edu>
2023-09-10 23:19:25 -0600

    fixed hsl wheel

--- hsl.js
+++ hsl.js
@@ -5,9 +5,34 @@ const hueSlider = document.querySelector("#hue-slider"),
   hexText = document.querySelector(".hex"),
   hslText = document.querySelector(".hsl"),
   colorBox = document.querySelector(".color-box"),
-  wheel = document.querySelector("canvas.color-wheel");
+  radius = 100,
+  ctxWheel = document.querySelector("canvas.color-wheel").getContext("2d"),
+  ctxPoint = document.querySelector("canvas.color-point").getContext("2d"),
+  pointRadius = 4;
 
-function hslToHex(h, s, l) {
+  let hasAlpha = false;
+
+const convertRadiansToDegrees = rad => {
+  return (rad + Math.PI) * 180 / Math.PI;
+}
+
+const convertDegreesToRadians = deg => {
+  return deg * Math.PI / 180;
+}
+
+const convertCartesianToPolar = (x, y) => {
+  const radius = Math.sqrt(x * x + y * y);
+  const theta = Math.atan2(y, x);
+  return [radius, theta];
+};
+
+const convertPolarToCartesian = (radius, theta) => {
+  const x = radius * Math.cos(theta) + radius;
+  const y = radius * Math.sin(theta) + radius;
+  return [x, y];
+};
+
+const hslToHex = (h, s, l) => {
   l /= 100;
   const a = (s * Math.min(l, 1 - l)) / 100;
   const f = (n) => {
@@ -19,6 +44,18 @@ function hslToHex(h, s, l) {
   };
   return `#${f(0)}${f(8)}${f(4)}`;
 }
+
+// adapted from https://en.wikipedia.org/wiki/HSL_and_HSV#HSL_to_RGB
+  // expected hue range: [0, 360]
+  // expected saturation range: [0, 1]
+  // expected lightness range: [0, 1]
+  const HSLToRGB = (h, s, l) => {
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = n =>
+      l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    return [255 * f(0), 255 * f(8), 255 * f(4)];
+  };
 
 function updateColor() {
   hasAlpha = alphaSlider.value < 255;
@@ -34,13 +71,20 @@ function updateColor() {
     hsl = `hsl(${hslInner})`;
   }
 
-  hexText.innerHTML = hex;
+  hexText.textContent = hex;
   colorBox.style.backgroundColor = hsl;
-  hslText.innerHTML = hsl;
+  hslText.textContent = hsl;
+
+  const [x,y] = convertPolarToCartesian(radius * satSlider.value / 100, convertDegreesToRadians(hueSlider.value - 180));
+  ctxPoint.strokeStyle = "#000";
+  ctxPoint.clearRect(0, 0, radius*2, radius*2);
+  ctxPoint.beginPath();
+  ctxPoint.arc(x + radius - satSlider.value, y + radius - satSlider.value, pointRadius, 0, 2 * Math.PI);
+  ctxPoint.stroke();
 }
 
 function bindInputs(slider, text, hue = false) {
-  max = 100;
+  let max = 100;
   if (hue) {
     max = 360;
     slider.addEventListener("input", () => {
@@ -68,32 +112,18 @@ function bindInputs(slider, text, hue = false) {
   });
 }
 
-bindInputs(hueSlider, document.querySelector("#hue-value"), true);
-bindInputs(satSlider, document.querySelector("#sat-value"));
-bindInputs(lightSlider, document.querySelector("#light-value"));
-alphaSlider.addEventListener("input", updateColor);
-
-
-updateColor();
-
-// new
-const radius = 100;
-  let ctx = wheel.getContext("2d");
-  drawHSLCircle(radius);
-
-  function drawHSLCircle(radius) {
-    const image = ctx.createImageData(2 * radius, 2 * radius);
+function drawHSLCircle(radius) {
+    const image = ctxWheel.createImageData(2 * radius, 2 * radius);
     const data = image.data;
     const rowLength = 2 * radius;
     const pixelWidth = 4; // each pixel requires 4 spaces in the data array (RGBA)
     const alpha = 255;
-    const value = 1.0;
+    const value = .5;
 
-    for (let y = -radius; y <= radius; y++) {
+    for (let x = -radius; x <= radius; x++) {
+        for (let y = -radius; y <= radius; y++) {
 
-        for (let x = -radius; x <= radius; x++) {
-
-        const [distance, theta] = convertCoordinatesToPolar(x, y);
+        const [distance, theta] = convertCartesianToPolar(x, y);
 
         // process only (x,y) coordinates that are inside circle
         if (distance <= radius) {
@@ -102,39 +132,31 @@ const radius = 100;
 
             const saturation = distance / radius;
 
-            const [red, green, blue] = hsvToRgb(hue, saturation, value);
+            // const [red, green, blue] = hsvToRgb(hue, saturation, value);
+            let [red, green, blue] = HSLToRGB(hue, saturation, value);
 
             // find index in data array
-            const index = ((x + radius) * rowLength + y + radius) * pixelWidth;
+            const index = (x + radius + (y + radius) * rowLength) * pixelWidth;
 
             data[index] = red;
             data[index + 1] = green;
             data[index + 2] = blue;
             data[index + 3] = alpha;
         }
-      }
+    }
     }
 
-    ctx.putImageData(image, 0, 0);
-  }
-
-  function convertCoordinatesToPolar(x, y) {
-    const radius = Math.sqrt(x * x + y * y);
-    const theta = Math.atan2(y, x);
-    return [radius, theta];
-  }
-
-  // rad in [-π, π] range
-  // return degree in [0, 360] range
-  function convertRadiansToDegrees(rad) {
-      return (rad + Math.PI) * 180 / Math.PI;
+    ctxWheel.putImageData(image, 0, 0);
 }
 
-
-// hue in range [0, 360]
-// saturation, value in range [0,1]
-// return [r,g,b] each in range [0,255]
-// See: https://en.wikipedia.org/wiki/HSL_and_HSV#From_HSV
+/**
+ * Convert HSV to RGB value
+ * See: https://en.wikipedia.org/wiki/HSL_and_HSV#From_HSV
+ * @param {float} hue
+ * @param {float} saturation
+ * @param {float} value
+ * @returns [r,g,b] each in range [0,255]
+ */
 function hsvToRgb(hue, saturation, value) {
   let chroma = value * saturation;
   let hue1 = hue / 60;
@@ -160,3 +182,22 @@ function hsvToRgb(hue, saturation, value) {
   // Change r,g,b values from [0,1] to [0,255]
   return [255 * r, 255 * g, 255 * b];
 }
+
+
+
+
+
+
+
+ctxPoint.globalCompositeOperation = "destination-over";
+bindInputs(hueSlider, document.querySelector("#hue-value"), true);
+bindInputs(satSlider, document.querySelector("#sat-value"));
+bindInputs(lightSlider, document.querySelector("#light-value"));
+alphaSlider.addEventListener("input", updateColor);
+
+
+drawHSLCircle(radius);
+updateColor();
+
+
+
